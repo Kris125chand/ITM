efca12dbcf686e5fd735c5f7a7d9dac28e50348a
Kristina Chandran <kris125chand@gmail.com>
2024-07-24 14:50:49 +0300

    Выполнена задача без использования пакета bytes, добавлен тест с ссылкой до конца строки и имена кейсов

--- main_test.go
+++ main_test.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestMask(t *testing.T) {
-    tests := []struct {
-        input  string
-        output string
-    }{
-        {"Here's my spammy page: http://hehefouls.netHAHAHA see you.", "Here's my spammy page: http://******************* see you."},
-        {"No URL here.", "No URL here."},
-        {"Visit http://example.com for more info.", "Visit http://*********** for more info."},
-        {"Много ссылок в одной строке http://link1.com и http://link2.com", "Много ссылок в одной строке http://********* и http://*********"},
-    }
+	tests := []struct {
+		name   string
+		input  string
+		output string
+	}{
+		{"WithURL", "Here's my spammy page: http://hehefouls.netHAHAHA see you.", "Here's my spammy page: http://******************* see you."},
+		{"NoURL", "No URL here.", "No URL here."},
+		{"SimpleURL", "Visit http://example.com for more info.", "Visit http://*********** for more info."},
+		{"MultipleURLs", "Много ссылок в одной строке http://link1.com и http://link2.com", "Много ссылок в одной строке http://********* и http://*********"},
+		{"EndSpace", "http://link1.com ", "http://********* "},
+		{"EndWithURL", "http://link1.com", "http://*********"},
+	}
 
-    for _, test := range tests {
-        result := mask(test.input)
-        if result != test.output {
-            t.Errorf("For input %q, expected %q, but got %q", test.input, test.output, result)
-        }
-    }
+	for _, test := range tests {
+		result := mask(test.input)
+		if result != test.output {
+			t.Errorf("Test %s failed: For input %q, expected %q, but got %q", test.name, test.input, test.output, result)
+		}
+	}
 }
